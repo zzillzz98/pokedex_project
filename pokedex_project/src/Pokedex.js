f359d1e3199e4091fa22f0e0a53820e1d64f6caa
rahimz98 <rahimz@student.unimelb.edu.au>
2020-06-14 18:05:02 +1000

    setting up node and adding the base pages

---
+++ pokedex_project/src/Pokedex.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pokedex = () => {
+    return (
+        <div>
+            This is the pokedex page
+        </div>
+    )
+}
+
+export default Pokedex;
---
+++ pokedex_project/src/Pokemon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pokemon = () => {
+    return (
+        <div>
+            This is the pokemon page
+        </div>
+    )
+}
+
+export default Pokemon;
